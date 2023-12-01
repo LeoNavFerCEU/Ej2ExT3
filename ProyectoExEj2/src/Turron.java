77841ed4d1cc1afaff030dadf692ac738eea246b
LeoNavFerCEU <lnavarro8494@ceuandalucia.es>
2023-12-01 19:47:35 +0100

    Mi preferido es el de chocolate.

--- ProyectoExEj2/src/Turron.java
+++ ProyectoExEj2/src/Turron.java
@@ -4,6 +4,7 @@ public class Turron {
 	public static void main(String[] args) {
 		
 		System.out.println("Soy Leo y me encantan los turrones.");
+		System.out.println("Pero mi preferido es el de chocolate");
 
 	}
 
